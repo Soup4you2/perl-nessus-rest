f872ed45f13e8468062ccc097d4819bc2c988342
renfrow <renfrow@studentclearinghouse.org>
2014-12-03 11:57:54 -0500

    First commit

---
+++ Nessus_REST.pm
@@ -0,0 +1,419 @@
+#!/usr/bin/perl
+
+use strict;
+use LWP;
+use LWP::UserAgent;
+use JSON;
+use Data::Dumper;
+use Term::ANSIColor qw(:constants);
+
+# Self signed cert, so don't verify the host info.
+BEGIN { $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0 }
+
+# Config Variables
+my $apibaseurl = 'https://127.0.0.1:8834/';
+my $username = 'username';
+my $password = 'password';
+
+# Create LWP User agent (web browser)
+my $ua = LWP::UserAgent->new;
+$ua->agent("OSC/0.1");
+
+sub login {
+	print GREEN, "\n\n***" . YELLOW, " Logging into Nessus server \n";
+	# Create the POST request that sends the username and password
+	my $req = HTTP::Request->new(POST => $apibaseurl . 'session');
+	$req->content_type('application/x-www-form-urlencoded');
+	$req->content("username=$username&password=$password");
+
+	# Send the request to the Nessus API  ( /session )
+	our $res = $ua->request($req);
+
+	# See is the login worked, and if so get the session token
+	our $NessusToken;
+	if ($res->is_success) {
+		my $result = from_json($res->content);
+		$NessusToken = $result->{'token'};
+	} else {
+		print $res->status_line . "\n";
+		print $res->content . "\n";
+		exit;
+	};
+
+	print GREEN, "***" . YELLOW, " Received Auth Token:" . CYAN, " $NessusToken \n";
+
+	# Update headers to include token
+	our $h = new HTTP::Headers;
+	$h->header('X-Cookie' => "token=$NessusToken;");
+}
+
+use vars qw( $res $NessusToken $apibaseurl $h );
+
+sub get_policy {	
+	# Display the policies (/policies)
+	my $req = HTTP::Request->new('GET' , $apibaseurl . 'policies' , $h);
+	$req->content_type('application/json; charset=UTF-8');
+
+	# Send the request to the server
+	$res = $ua->request($req);
+
+	# Test for request failure
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+
+	# Convert JSON data to Perl data structure
+	my $policydata = from_json($res->content);
+
+	#print BLUE, Dumper($policydata);
+	print WHITE, "\n" . "="x75 . MAGENTA, "\n\t\t\t\tPolicy List \n" . WHITE, "="x75 . "\n";
+	print WHITE, "ID \t Name \t\t\t Description \t\t UUID \n" . "-"x75 . "\n";
+	
+	foreach my $x (@{$policydata->{'policies'}}) {
+		print CYAN, "$x->{'id'} \t $x->{'name'} \t\t\t $x->{'description'} \t\t\t $x->{'template_uuid'} \n";	
+	};
+}
+
+sub get_scans {
+	# List the Scans (/scans)
+	my $req = HTTP::Request->new('GET' , $apibaseurl . 'scans' , $h);
+	$req->content_type('application/json; charset=UTF-8');
+	
+	# Send the request to the server
+	$res = $ua->request($req);
+	
+	# Test for failute
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+	
+	# Convert JSON data to Perl data structure
+	my $scandata = from_json($res->content);
+	
+	#print BLUE, Dumper($scandata);
+	print WHITE, "\n" . "="x75 . MAGENTA, "\n\t\t\t\tScan List \n" . WHITE, "="x75 . "\n";	
+	print RESET, "ID \t Name \t\t\t Status \t\t  \n" . "-"x75 . "\n";
+
+	foreach my $x (@{$scandata->{'scans'}}) {
+		print "$x->{'id'} \t $x->{'name'} \t\t $x->{'status'} \n";
+	}
+}
+
+sub get_historyID {
+
+	# Check if scan_id was passed
+	if ($ARGV[0] eq '') {
+		warn RED, "\n*** ERROR: " . YELLOW, "Expected <scan_id> \n\n";
+		die;
+	};
+	
+	my $req = HTTP::Request->new('GET' , $apibaseurl . "scans/${ARGV[0]}" , $h);
+	$req->content_type('application/json; charset=UTF-8');
+	
+	# Send the request
+	$res = $ua->request($req);
+	
+	# Test for failure
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+	
+	# Convert JSON data to Perl data structure
+	my $historydata = from_json($res->content);
+
+	#print BLUE, Dumper($historydata);
+	print WHITE, "\n" . "="x75 . MAGENTA, "\n\t\t\tScan History for Scan ID:" . CYAN, " $ARGV[0]" . "\n" . WHITE, "="x75 . "\n";
+	print WHITE, "ID \t Status \t\t UUID\n" . "-"x75 . "\n";
+	foreach my $x (@{$historydata->{'history'}}) {
+		print CYAN, "$x->{'history_id'} \t $x->{'status'} \t\t $x->{'uuid'} \n";
+	};
+	
+}
+
+sub get_scan_export {
+	# Check if scan_id was passed
+	if ($ARGV[0] eq '') {
+		warn RED, "\n*** ERROR: " . YELLOW, "Expected <scan_id> \n\n";
+		die;
+	};
+	
+	print GREEN, "\n*** " . YELLOW, "Exporting Scan ID " . CYAN, "$ARGV[0]" . YELLOW, " To Nessus Format \n";
+	
+	my $req = HTTP::Request->new('POST' , $apibaseurl . "scans/${ARGV[0]}/export" , $h);
+	$req->content_type('application/x-www-form-urlencoded');
+	$req->content("scan_id=${ARGV[0]}&format=nessus");
+	
+	# Send the request
+	$res = $ua->request($req);
+	
+	# Test for failure
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+	
+	# Convert JSON data to Perl data structure
+	my $postdata = from_json($res->content);
+	
+	#print BLUE, Dumper($postdata);
+	#print GREEN, "*** " . YELLOW, "Exported to file ID: ";
+	print RESET;
+	print "*** Exported to file ID: $postdata->{'file'} \n";
+	#print "$postdata->{'file'} \n";
+	
+	# Set a variable for later use.
+	our $exportID=$postdata->{'file'};
+};
+
+sub get_export_status {
+	
+	use vars qw( $exportID );
+	
+	my $req = HTTP::Request->new('GET' , $apibaseurl . "scans/${ARGV[0]}/export/${exportID}/status" , $h);
+	$req->content_type('application/json; charset=UTF-8');
+	
+	# Send the request to the server
+	$res = $ua->request($req);
+	
+	# Test for failute
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}	
+
+	#print Dumper($res->content);
+	my $exportstatus = from_json($res->content);
+	#print GREEN, "***" . YELLOW " Current Export Status: " . CYAN, "$exportstatus->{'status'} \n";
+	print RESET;
+	print "*** Current Export Status: $exportstatus->{'status'} \n";
+	
+	if ($exportstatus->{'status'} ne "ready") {
+		sleep(2);
+		&get_export_status;
+	};
+}
+
+sub get_scan_download {
+
+	#use vars qw( $exportID );
+
+	print GREEN, "***" . YELLOW, " Downloading File ID: " . CYAN, "${ARGV[1]}" . YELLOW, " For Scan ID: " . CYAN, "$ARGV[0] \n"; 
+	my $req = HTTP::Request->new('GET' , $apibaseurl . "scans/${ARGV[0]}/export/${ARGV[1]}/download" , $h);
+	$req->content_type('application/json; charset=UTF-8');
+	
+	# Send the request to the server
+	$res = $ua->request($req);
+
+	#print "*** DEBUGGING: Sending Request \n";
+	#print Dumper($req);
+	# Test for failute
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}	
+
+	# Output results to a a file
+	if ($ARGV[1] eq '') {
+		warn RED, "\n*** ERROR: " . YELLOW, "Expected <scan_id> <output_file> \n\n";
+		die;	
+	}
+	
+	#print "*** DEBUGGING: RESPONSE RECEIVED\n";
+	#print Dumper($res);
+
+	#print "*"x80 . "\n";
+	#print Dumper($res->content);
+
+	print GREEN, "***" . YELLOW, " Saving Nessus XML v2 format file as: " . CYAN, "$ARGV[2]" . "\n";
+	open(FILE, "> $ARGV[2]") or error_msg("Failed to write report file $ARGV[2]: $!");
+	print FILE $res->content;
+	close FILE;
+
+}
+
+sub get_scanners {
+	# List the Scanners (/scanners)
+	my $req = HTTP::Request->new('GET' , $apibaseurl . 'scanners' , $h);
+	$req->content_type('application/json; charset=UTF-8');
+	
+	# Send the request to the server
+	$res = $ua->request($req);
+	
+	# Test for failute
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+	
+	# Convert JSON data to Perl data structure
+	my $scannerdata = from_json($res->content);
+	
+	#print BLUE, Dumper($scannerdata);
+	print WHITE, "\n" . "="x75 . MAGENTA, "\n\t\t\t\tScanner List \n" . WHITE, "="x75 . "\n";	
+	print WHITE, "ID \t Name \t\t\t UUID \n" . "-"x75 . "\n";
+
+	foreach my $x (@{$scannerdata}) {
+		print CYAN, "$x->{'id'} \t $x->{'name'} \t\t $x->{'uuid'} \n";
+	}	
+
+}
+
+sub get_folders {
+	# List the folders (/folders)
+	my $req = HTTP::Request->new('GET' , $apibaseurl . 'folders' , $h);
+	$req->content_type('application/json; charset=UTF-8');
+	
+	# Send the request to the server
+	$res = $ua->request($req);
+	
+	# Test for failute
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+	
+	# Convert JSON data to Perl data structure
+	my $folderdata = from_json($res->content);
+	
+	#print BLUE, Dumper($folderdata);
+	print WHITE, "\n" . "="x75 . MAGENTA, "\n\t\t\t\tFolder List \n" . WHITE, "="x75 . "\n";	
+	print WHITE, "ID \t Name \n" . "-"x75 . "\n";
+
+	foreach my $x (@{$folderdata->{'folders'}}) {
+		print CYAN, "$x->{'id'} \t $x->{'name'} \n";
+	}	
+
+}
+
+sub get_policy_templates {
+	# List the templates (/folders)
+	my $req = HTTP::Request->new('GET' , $apibaseurl . 'editor/policy/templates' , $h);
+	$req->content_type('application/json; charset=UTF-8');
+	
+	# Send the request to the server
+	$res = $ua->request($req);
+	
+	# Test for failute
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+	
+	# Convert JSON data to Perl data structure
+	my $folderdata = from_json($res->content);
+	
+	#print BLUE, Dumper($folderdata);
+	print WHITE, "\n" . "="x75 . MAGENTA, "\n\t\t\t\tPolicy Template List \n" . WHITE, "="x75 . "\n";	
+	print WHITE, "Title \t\t\tUUID \n" . "-"x75 . "\n";
+
+	foreach my $x (@{$folderdata->{'templates'}}) {
+		print CYAN, "$x->{'title'} \t\t\t $x->{'uuid'} \n";
+	}	
+
+}
+
+sub create_nessus_scan {
+	
+	if ($ARGV[5] eq '') {
+		warn RED, "\n*** ERROR: " . YELLOW, "Expected <template_policy_uuid> <scan_label> <folder_id> <policy_id> <scanner_id> <targets_file>\n\n";
+		die;
+	};	
+	
+	print GREEN, "*** " . YELLOW, "Creating new Scan " . CYAN, "$ARGV[1]" . YELLOW, " \n";
+	
+
+	# Post to (/scans)
+	my $req = HTTP::Request->new('POST' , $apibaseurl . "scans" , $h);
+	$req->content_type('application/json; charset=UTF-8');
+
+	# Generate the JSON POST data.
+	my $json = '{
+		"uuid": "'.$ARGV[0].'", 
+		"settings": {
+			"name": "'.$ARGV[1].'", 
+			"launch": "ON_DEMAND",
+			"folder_id": "'.$ARGV[2].'",
+			"policy_id": "'.$ARGV[3].'",
+			"scanner_id": "'.$ARGV[4].'",
+			"text_targets": "'.$ARGV[5].'"
+		}
+	}';
+
+	# Populate the BODY with JSON encoded data.
+	$req->content($json);
+	
+	# Send the request
+	$res = $ua->request($req);
+	#print Dumper($req);
+	
+	# Test for failure
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+	
+	# Convert JSON data to Perl data structure
+	my $postdata = from_json($res->content);
+	#print BLUE, Dumper($postdata);
+}
+
+sub launch_nessus_scan {
+	# Check if scan_id was passed
+	if ($ARGV[0] eq '') {
+		warn RED, "*** ERROR: " . YELLOW, "Expected <scan_id> \n\n";
+		die;
+	};
+	
+	print GREEN, "*** " . YELLOW, "Launching Scan ID: " . CYAN, "$ARGV[0]" . YELLOW, " \n";
+	
+	# Post to (/scans/{scan_id}/launch
+	my $req = HTTP::Request->new('POST' , $apibaseurl . "scans/${ARGV[0]}/launch" , $h);
+	$req->content_type('application/x-www-form-urlencoded');
+	$req->content("scan_id=${ARGV[0]}");
+	
+	# Send the request
+	$res = $ua->request($req);
+	
+	#print Dumper($req);
+	
+	# Test for failure
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+	
+}
+
+sub logoff {
+	#print GREEN, "\n*** " . YELLOW, "Destroying Session: " . CYAN, "$NessusToken" . YELLOW, " \n";
+	
+	# Post to (/session
+	my $req = HTTP::Request->new('DELETE' , $apibaseurl . "session" , $h);
+	$req->content_type('application/x-www-form-urlencoded');
+	
+	# Send the request
+	$res = $ua->request($req);
+	
+	# Test for failure
+	if (!$res->is_success) {
+		warn $res->status_line . "\n";
+		warn $res->content . "\n";
+		exit
+	}
+}
+
+1;
---
+++ REST_runner.pl
@@ -0,0 +1,165 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use Data::Dumper;
+use JSON;
+
+#$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
+
+require "Nessus_REST.pm";
+
+# Declair variables to keep strict happy
+my $list_policies;
+my $list_scans;
+my $get_historyID;
+my $list_scanners;
+my $list_folders;
+my $list_policy_templates;
+my $scan_export;
+my $scan_download;
+my $create_scan;
+my $launch_scan;
+my $help;
+my $n;
+my $o;
+my $x;
+my $y;
+my $z;
+
+### evaluate parameters
+GetOptions ("list-policies"   		=> \$list_policies,
+			"list-scans"      		=> \$list_scans,
+			"get-historyID"   		=> \$get_historyID,
+			"list-scanners"	  		=> \$list_scanners,
+			"list-folders"	  		=> \$list_folders,
+			"list-policy-templates"	=> \$list_policy_templates,
+			"scan-export"			=> \$scan_export,
+			"scan-download"			=> \$scan_download,
+			"create-scan"			=> \$create_scan,
+			"launch-scan"			=> \$launch_scan,
+			"help"					=> \$help
+);
+
+if($help) { 
+	print_help(); 
+}
+elsif($list_policies) { 
+	list_policies(); 
+}
+elsif($list_scans) { 
+	list_scans(); 
+}
+elsif($get_historyID) {
+	list_historyID($n);
+}
+elsif($list_scanners) {
+	list_scanners();
+}
+elsif($list_folders) {
+	list_folders();
+}
+elsif($list_policy_templates) {
+	list_policy_templates();
+}
+elsif($scan_export) {
+	scan_export($n);
+}
+elsif($scan_download) {
+	scan_download($n,$x,$y);
+}
+elsif($create_scan) {
+	create_scan($n,$o,$x,$y,$z);
+}
+elsif($launch_scan) {
+	launch_scan($x);
+}
+
+sub print_help {
+	print qq(
+		usage: $0 <command> [ <command-options> ]
+
+		Commands:
+		--------------------------------------------------------------------------------------------------------
+		--list-policies						- List the user created scan policies
+		--list-scans						- List scan history and status
+		--get-historyID						- List the history of a given scan
+			<scan_id>
+		--list-scanners						- List all the scanners
+		--list-folders						- List the available scan folders
+		--list-policy-templates					- List of pre-configured policy templates
+		--scan-export 						- Export a scan to Nessus XML v2 Format
+			<scan_id>
+		--scan-download						- Download a scan after an export
+			<scan_id> <export_file_id> <save as>
+		--create-scan 						- Define a new ON DEMAND scan
+			<template_policy_uuid> <scan_label> <folder_id> <policy_id> <scanner_id> <file_targets>
+		--launch-scan						- Launch an on demand scan
+			<scan_id> \n
+	);
+}
+
+sub list_policies {
+	&login;
+	&get_policy;
+	&logoff;
+}
+
+sub list_scans {
+	&login;
+	&get_scans;
+	&logoff;	
+}
+
+sub list_historyID {
+	&login;
+	&get_historyID($n);
+	&logoff;	
+}
+
+sub list_scanners {
+	&login;
+	&get_scanners;
+	&logoff;	
+}
+
+sub list_folders {
+	&login;
+	&get_folders;
+	&logoff;	
+}
+
+sub list_policy_templates {
+	&login;
+	&get_policy_templates;
+	&logoff;	
+}
+
+sub scan_export {
+	&login;
+	&get_scan_export($n);
+	&get_export_status;
+	&logoff;	
+}
+
+sub scan_download {
+	&login;
+	&get_scan_download($n,$x,$y);
+	&logoff;	
+}
+
+sub create_scan {
+	&login;
+	&create_nessus_scan($n,$o,$x,$y,$z);
+	&logoff;	
+}
+sub launch_scan {
+	&login;
+	&launch_nessus_scan($x);
+	&logoff;	
+}
+
+#print "\n";
+
+1;
+
